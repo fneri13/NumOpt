3f12de8e9b550e10a6e480fa235372efea1d0233
Francesco Neri <francesconeri95@gmail.com>
2025-11-18 11:33:31 +0100

    new ex

--- NumOpt/functions.py
+++ NumOpt/functions.py
@@ -2,6 +2,14 @@ def rosenbrock(x, a=1, b=100):
     x1, x2 = x
     return (a - x1)**2 + b*(x2 - x1**2)**2
 
+
 def himmelblau(x):
     x1, x2 = x
     return (x1**2 + x2 - 11)**2 + (x1 + x2**2 - 7)**2
+
+
+def beanFunction(x):
+    x1, x2 = x
+    f = (1 - x1)**2 + (1-x2)**2 + 0.5 *(2*x2-x1**2)**2
+    return f
+
